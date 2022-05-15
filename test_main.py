1604b47ea99a09bb6161cdea86ea4f94cb488d4c
remy-r <remi@perso-cloud.eu>
2022-05-15 11:20:39 +0200

    add config

---
+++ test_main.py
@@ -0,0 +1,12 @@
+"""Module Test solve Sudoku"""
+from main import sudoku_solver
+
+
+def test_solve_sudoku() -> None:
+    """Test sudoku"""
+    assert (
+        sudoku_solver(
+            "600837001089004700102000400000450020030609005040000860908006070700098010005100930"
+        )
+        == "654837291389214756172965483896451327237689145541723869918346572723598614465172938"
+    )
